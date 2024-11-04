3c010e7721550ad054df7c6a177459031c4f432f
dev-hanzala <160335647+dev-hanzala@users.noreply.github.com>
2024-11-04 22:10:14 +0500

    Finishing touches

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Home = () => {
   return (
-    <div className="flex justify-center mt-28 mx-28 space-x-32">
+    <div className="flex justify-center my-[118px] mx-28 space-x-32">
       <div className="flex-1">
         <h1 className="font-libre-bodoni font-bold text-[#000000] text-[38px] mt-[46px] mr-60 mb-12">
           IMPECCABLE CRAFTSMANSHIP AND FINESSE
@@ -11,17 +11,20 @@ const Home = () => {
           An example of intricate workmanship and detail, elegant necklaces and
           long and short chains form a part of our desirable collection.
         </p>
-        <div className="bg-[#A29875] text-white flex justify-center items-center rounded-lg w-60 h-12 text-2xl font-libre-bodoni font-medium">
+        <div className="bg-[#A29875] text-white flex justify-center items-center rounded-lg w-[248px] h-[50] text-[26px]/[32px] font-libre-bodoni font-medium">
           <p>Explore Now</p>
         </div>
       </div>
-      <Image
-        src="/images/hero.png"
-        alt="Hero Image"
-        width={400}
-        height={560}
-        className="rounded-tl-[120px] rounded-br-[120px]"
-      />
+      <div className="relative">
+        <div className="absolute top-6 left-5 right-5 bottom-6 border-[0.88px] border-white rounded-tl-[128px] rounded-br-[128px]"></div>
+        <Image
+          src="/images/hero.png"
+          alt="Hero Image"
+          width={421}
+          height={574}
+          className="rounded-tl-[128px] rounded-br-[128px] w-[421px] h-[574px]"
+        />
+      </div>
     </div>
   );
 };
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,54 +2,47 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <header className="bg-[#A29875] shadow-md">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="flex items-center space-x-4">
-          <Image
-            src="/vectors/logo.svg"
-            alt="Logo"
-            width={64}
-            height={70}
-            className="rounded-full"
-          />
-          <h1 className="text-[68px] font-rye text-white">urity</h1>
-        </div>
-        <div className="bg-[#F8F8F8] p-2 rounded-lg flex items-center w-1/2 relative">
-          <input
-            type="text"
-            className="w-full p-2 bg-transparent"
-            placeholder="Search for Gold Jewellery, Diamond Jewellery and more…"
-          />
-          <Image
-            src="/vectors/search.svg"
-            alt="Search Icon"
-            width={20}
-            height={20}
-            className="absolute right-2"
-          />
-        </div>
-        <div className="flex space-x-10">
-          <Image
-            src="/vectors/favourites.svg"
-            alt="Favourites Icon"
-            width={28}
-            height={28}
-          />
-          <Image
-            src="/vectors/profile.svg"
-            alt="Profile Icon"
-            width={28}
-            height={28}
-          />
-          <Image
-            src="/vectors/cart.svg"
-            alt="Cart Icon"
-            width={28}
-            height={28}
-          />
-        </div>
+    <div className="bg-[#A29875] flex justify-between items-center py-4 px-[56px]">
+      <div className="flex items-center space-x-1">
+        <Image
+          src="/vectors/logo.svg"
+          alt="Logo"
+          width={64}
+          height={70}
+          className="rounded-full"
+        />
+        <h1 className="text-[68px] font-rye text-white m-0">urity</h1>
       </div>
-    </header>
+      <div className="bg-[#F8F8F8] p-2 rounded-lg flex items-center w-1/2 relative">
+        <input
+          type="text"
+          className="w-full p-2 bg-transparent"
+          placeholder="Search for Gold Jewellery, Diamond Jewellery and more…"
+        />
+        <Image
+          src="/vectors/search.svg"
+          alt="Search Icon"
+          width={20}
+          height={20}
+          className="absolute right-2"
+        />
+      </div>
+      <div className="flex space-x-10">
+        <Image
+          src="/vectors/favourites.svg"
+          alt="Favourites Icon"
+          width={28}
+          height={28}
+        />
+        <Image
+          src="/vectors/profile.svg"
+          alt="Profile Icon"
+          width={28}
+          height={28}
+        />
+        <Image src="/vectors/cart.svg" alt="Cart Icon" width={28} height={28} />
+      </div>
+    </div>
   );
 };
 
